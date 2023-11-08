644eb87e913da39070c8709687c2d414706ce65d
Pablo <pablogcg05@gmail.com>
2023-11-08 23:07:04 +0100

    Últimos cambios

--- prueba.py
+++ prueba.py
@@ -1,9 +1,37 @@
-name = input("Please introduce your surname: ")
-numbers= ["0","1,","2","3","4","5","6","6","7","8","9"]
-flag_name= True
-for a in name:
-    for p in numbers:
-            if a == p:
-                flag_name= False
-if flag_name== False:
-     print("You can not enter a number")
+z = 0
+while z == 0:
+    x = 0
+    y = 0
+    name = input("Introduce your name:")
+    j = ("0123456789")
+    while x < len(name):
+        if y < 10:
+            if j[y] == name[x]:
+                print("The field only accepts letters")
+                x = x+len(name)
+            else:
+                y = y+1
+        else:
+            x = x+1
+            y = 0
+        if len(name) == x:
+            z = 1
+
+z = 0
+while z == 0:
+    x = 0
+    y = 0
+    surname = input("Introduce your surname: ")
+    j = "0123456789"
+    while x < len(surname):
+        if y < 10:
+            if j[y] == surname[x]:
+                print("The field only accepts letters")
+                x = x + len(surname)
+            else:
+                y = y + 1
+        else:
+            x = x + 1
+            y = 0
+        if len(surname) == x:
+            z = 1
